915c9001df54bba0de8520d553765425a42465b8
Matheus Alves <matheus.jeje2@gmail.com>
2025-09-29 10:52:53 -0300

    Reiniciando time pós responder uma questão

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,36 @@
-import { useState } from 'react'
+// app.jsx: (Código MODIFICADO)
+import { useState, useEffect } from 'react' // Importe useEffect
 import './App.module.css'
 import Header from "./components/Header"
 import QuestionCard from "./components/QuestionCard"
 
 function App() {
-  const [currentIndex, setCurrentIndex] = useState(0)
+    const [currentIndex, setCurrentIndex] = useState(0)
+   
+    const [time, setTime] = useState(0)
 
-  return (
-    <>
-      <Header index={currentIndex}/>
-      <QuestionCard index={currentIndex} onClickButton={() => setCurrentIndex(currentIndex + 1)} />
-    </>
-  )
+    useEffect(() => {
+        
+        setTime(0)
+
+        const interval = setInterval(() => {
+            setTime((prev) => prev + 1)
+        }, 1000)
+
+        
+        return () => clearInterval(interval)
+ 
+    }, [currentIndex]) 
+
+    return (
+        <>
+            <Header index={currentIndex} time={time}/>
+            <QuestionCard 
+                index={currentIndex} 
+                onClickButton={() => setCurrentIndex(currentIndex + 1)} 
+            />
+        </>
+    )
 }
 
 export default App
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,28 +1,19 @@
+
 import {question} from "../../data/questions"
 import styles from "./Header.module.css"
 
-import { useEffect, useState } from "react"
 
-export default function Header({index}) {
-    const [time, setTime] = useState(0) 
+export default function Header({index, time}) { 
+    
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setTime((prev) => prev + 1)
-    }, 1000)
+    const minutes = Math.floor(time / 60)
+    const seconds = time % 60
 
-    return () => clearInterval(interval)
-  }, [])
-
-  const minutes = Math.floor(time / 60)
-  const seconds = time % 60
-
-  return (
-    <header className={styles.header}>
-      <h1>Quiz Interativo</h1>
-      <h2>{`Questão ${question[index].id} de ${question.length}`}</h2>
-      <h3>Tempo: {minutes}:{seconds.toString().padStart(2, "0")}</h3>
-    </header>
-  )
+    return (
+        <header className={styles.header}>
+            <h1>Quiz Interativo</h1>
+            <h2>{`Questão ${question[index].id} de ${question.length}`}</h2>
+            <h3>Tempo: {minutes}:{seconds.toString().padStart(2, "0")}</h3>
+        </header>
+    )
 }
-
